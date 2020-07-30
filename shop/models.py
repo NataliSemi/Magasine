f6650f3df32d534286433b515f662c29bfcba5fc
nata654s <nata654s@yahoo.com>
2020-07-30 18:29:28 +0300

    Celery is workling

--- shop/models.py
+++ shop/models.py
@@ -35,3 +35,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
