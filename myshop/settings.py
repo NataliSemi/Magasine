de2ca5c069b9c3418cf06c8132022003df0e6cf3
nata654s <nata654s@yahoo.com>
2020-08-28 22:31:37 +0300

    first commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-
+import braintree
+import heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
